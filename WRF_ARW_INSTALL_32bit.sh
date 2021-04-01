3fc87e2073b3383dcc3455ac13f74b77174834c1
Will H <80460171+whatheway@users.noreply.github.com>
2021-04-01 15:15:53 -0500

    Update WRF_ARW_INSTALL_32bit.sh

--- WRF_ARW_INSTALL_32bit.sh
+++ WRF_ARW_INSTALL_32bit.sh
@@ -46,8 +46,6 @@ export CC=gcc
 export CXX=g++
 export FC=gfortran
 export F77=gfortran
-export FCFLAGS='-m64'
-export FFLAGS='-m64'
 
 #############################zlib############################
 cd $HOME/WRF/Downloads
