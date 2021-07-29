018d606ce5d2fac79ecc74fb9b8ce2cdeec3513a
Will H <80460171+whatheway@users.noreply.github.com>
2021-07-29 17:45:40 -0500

    Update WRF_ARW_INSTALL_32bit.sh
    Changin when MET & METplus get installed

--- WRF_ARW_INSTALL_32bit.sh
+++ WRF_ARW_INSTALL_32bit.sh
@@ -304,6 +304,14 @@ mkdir $HOME/WRF/WRFPortal
 unzip wrf-portal.zip -d $HOME/WRF/WRFPortal
 chmod +x $HOME/WRF/WRFPortal/runWRFPortal
 
+
+######################## DTC's MET & METplus ###########################
+## See script for details
+
+cd
+$HOME/MET_self_install_script_Linux_64bit.sh
+
+
 ######################## Static Geography Data inc/ Optional ####################
 # http://www2.mmm.ucar.edu/wrf/users/download/get_sources_wps_geog.html
 # Double check if Irrigation.tar.gz extracted into WPS_GEOG folder
@@ -334,8 +342,8 @@ tar -xvf lake_depth.tar.bz2 -C $HOME/WRF/GEOG/WPS_GEOG
 echo "export PATH=$DIR/bin:$PATH" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=$DIR/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
 
-cd
-$HOME/MET_self_isntall_script_Linux_32bit.sh
+
+
 
 #####################################BASH Script Finished##############################
 echo "Congratulations! You've successfully installed all required files to run the Weather Research Forecast Model verison 4.3."
