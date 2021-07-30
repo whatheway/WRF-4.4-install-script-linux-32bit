fb5936578046300f669814614395033e81d8520f
Will H <80460171+whatheway@users.noreply.github.com>
2021-07-29 19:47:30 -0500

    Update MET_self_install_script_Linux_32bit.sh
    fixing pathing

--- MET_self_install_script_Linux_32bit.sh
+++ MET_self_install_script_Linux_32bit.sh
@@ -54,7 +54,7 @@ export SET_D64BIT=FALSE
 ./compile_MET_all.sh 
 
 cd 
-$HOME/METplus_self_install_script_Linux_32bit.sh
+$HOME/WRF-4.3-install-script-linux-32bit/METplus_self_install_script_Linux_32bit.sh
 
 
 
